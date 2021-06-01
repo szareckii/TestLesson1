8bd32bcdf357ed30f324e3eb65eba439aba6f935
szareckii <zareckii@gmail.com>
2021-06-01 23:11:03 +0300

    Initial commit

---
+++ app/src/main/java/com/szareckii/testlesson1/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.szareckii.testlesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.szareckii.testlesson1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val emailValidator = EmailValidator()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            emailInput.addTextChangedListener(emailValidator)
+
+            saveButton.setOnClickListener {
+                if (emailValidator.isValid) {
+                    Toast.makeText(this@MainActivity, getString(R.string.valid_email), Toast.LENGTH_SHORT).show()
+                } else {
+                    val errorEmail = getString(R.string.invalid_email)
+                    emailInput.error = errorEmail
+                    Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
